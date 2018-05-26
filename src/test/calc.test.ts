3a027d439435a2fb8f0524731c4f827834776863
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-05-26 12:55:38 +0900

    the unit test now attempts to get fixture from file.

--- src/test/calc.test.ts
+++ src/test/calc.test.ts
@@ -1,12 +1,14 @@
 import 'mocha';
 import * as assert from 'power-assert';
 import * as fs from "fs";
+import * as path from "path";
 // @ts-ignore
 import * as arithmetics from "../main/arithmetics";
 
 describe("arithmetics", () => {
   it("should return addition of two operands.", () => {
-    const actual = arithmetics.parse("1 + 2");
+    const source = fs.readFileSync(path.resolve(__dirname, "fixture/calculation.calc"), "utf-8");
+    const actual = arithmetics.parse(source);
     assert.equal(actual, 3);
   });
 });
